e6bd60fafbcf342aaa3e401abda9e2085d66d26a
pranjali999 <pranjaliyeotikar2005@gmail.com>
2024-11-13 22:50:48 +0530

    Create customer.py

---
+++ customer.py
@@ -0,0 +1,22 @@
+class Customer:
+    def __init__(self, customer_id, name, address, phone_number, email):
+        self.customer_id = customer_id
+        self.name = name
+        self.address = address
+        self.phone_number = phone_number
+        self.email = email
+        self.accounts = []  # A list to store multiple accounts the customer has
+
+    def add_account(self, account):
+        self.accounts.append(account)
+
+    def view_balance(self):
+        for account in self.accounts:
+            print(f"Account Number: {account.account_number}, Balance: ${account.balance}")
+    
+    def deposit(self, account, amount):
+        account.deposit(amount)
+    
+    def withdraw(self, account, amount):
+        account.withdraw(amount)
+
